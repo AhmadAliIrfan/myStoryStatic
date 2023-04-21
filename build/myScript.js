7454dec270f7157ffe2a64c6bd9de045158e1d38
AhmadAliIrfan <73239577+AhmadAliIrfan@users.noreply.github.com>
2023-04-21 14:42:10 +0500

    Site Static

---
+++ build/myScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('.select-box').click(function(){
+
+	$('.select-box-clicked').removeClass('select-box-clicked');
+	$(this).addClass('select-box-clicked');
+})
+
+});
+
+
+
+
+
+
